b0243098c976acdd42018994e4b1432c32f6659b
Espe Cabrer <mecabrerv@gmail.com>
2021-09-17 12:57:00 +0200

    Ajustes estilo

--- public/js/script.js
+++ public/js/script.js
@@ -8,14 +8,14 @@ document.addEventListener(
       const navbarChange = document.getElementById("login-or-profile")
       navbarChange.innerHTML = `
             <a class="py-2 p-3 d-md-inline-block" href="/profile"><i class="fas fa-user"></i></a>
-            <a class="py-2 p-3 d-md-inline-block" href="/auth/logout">Logout</a>
+          <a class="py-2 p-3 d-md-inline-block" href="/auth/logout">Logout</a>
             `
     } else {
     
       const navbarChange = document.getElementById("login-or-profile")
       navbarChange.innerHTML = `
-            <a class="py-2 p-3 d-md-inline-block" href="/auth/signup">Registrarse</a>
-            <a class="py-2 p-3 d-md-inline-block" href="/auth/login">Iniciar Sesión</a>
+           <a class="py-2 p-3 nav-link " href="/auth/signup">Registrarse</a>
+          <a class="py-2 p-3  nav-link " href="/auth/login">Iniciar Sesión</a>
             `
     }
   },
